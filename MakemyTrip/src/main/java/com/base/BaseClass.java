ba37a53f0eea9688cf566cbaf7fd5bcc5648b2f9
gaurav.saini <gaurav.saini@DESKTOP-LJ905HE.ad.healthtell.com>
2026-04-27 14:33:30 -0700

    update 1

--- MakemyTrip/src/main/java/com/base/BaseClass.java
+++ MakemyTrip/src/main/java/com/base/BaseClass.java
@@ -1,17 +1,11 @@
 package com.base;
 
-import java.io.File;
 import java.time.Duration;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
--- MakemyTrip/src/main/java/com/pageObjectClasses/BookFlight.java
+++ MakemyTrip/src/main/java/com/pageObjectClasses/BookFlight.java
@@ -2,12 +2,9 @@ package com.pageObjectClasses;
 
 import java.time.Duration;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
